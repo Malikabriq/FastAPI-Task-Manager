73a9d5a4357d0a6c0ccaeca1d92cfe45aa1ee602
Muhammad Abriq <mrabriq756@gmail.com>
2025-11-10 12:02:58 +0500

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -1,18 +1,43 @@
 from .db import load_data, save_data
-from .models import Task
+from .models import Task, TaskUpdate
+
 
 def get_all():
     return load_data()
 
-def get(task_id):
+
+def get(task_id: str):
     data = load_data()
     return data.get(task_id)
+
 
 def create(task: Task):
     data = load_data()
     if task.id in data:
-        raise ValueError("Already exists")
+        raise ValueError("Task already exists")
     data[task.id] = task.model_dump(exclude={"id"})
     save_data(data)
 
-# update/delete similarly...
+def update(task_id: str, task_update: TaskUpdate):
+    data = load_data()
+    if task_id not in data:
+        raise ValueError("Task not found")
+    
+    existing_task = data[task_id]
+    updated_fields = task_update.model_dump(exclude_unset=True)
+
+    for key, value in updated_fields.items():
+        existing_task[key] = value
+
+    existing_task["id"] = task_id
+    updated_task = Task(**existing_task)
+    data[task_id] = updated_task.model_dump(exclude={"id"})
+    save_data(data)
+
+
+def delete(task_id: str):
+    data = load_data()
+    if task_id not in data:
+        raise ValueError("Task not found")
+    del data[task_id]
+    save_data(data)
